9d78fdec582abfba300da50a1afbe58572d63153
pontus393 <pontus.loefgren@gmail.com>
2023-06-30 11:44:56 +0200

    Declare functions for basic math operators.

---
+++ script.js
@@ -0,0 +1,17 @@
+// Declare functions for all of the basic math operators
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
